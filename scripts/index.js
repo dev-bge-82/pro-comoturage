3e6e1e16f16741dc9bdf079127b87a0e87f65e49
ShivaPrime82 <55974913+ShivaPrime82@users.noreply.github.com>
2020-02-06 10:14:36 +0100

    -- v0.0.6 --

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function cacherR(){
+	$("#formR").css("display","none");
+	$("#formP").css("display","block");
+	}
+function cacherP(){
+	$("#formP").css("display","none");
+	$("#formR").css("display","block");
+	}
