7b01faacfda7a08c58e848d51b084b2cde1a5167
ShivaPrime82 <55974913+ShivaPrime82@users.noreply.github.com>
2020-02-11 13:37:55 +0100

    -- v0.0.11 --

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 function cacherR(){
-	$("#formR").css("display","none");
-	$("#formP").css("display","block");
+	$("#formR").css("display","none"), //Quand on clique sur proposer, le formulaire disparaît.
+	$("#formP").css("display","block"), //Quand on clique sur proposer, le formulaire apparaît.
+	$("#btn_proposer").css("background-color","f3cbc4"), //Quand on clique sur le boutoun rechercher "cacherP", le texte devient vert.
+	$("#btn_rechercher").css("background-color","white") //Quand on clique sur le boutoun rechercher "cacherP", le texte devient noir.
 	}
-function cacherP(){
-	$("#formP").css("display","none");
-	$("#formR").css("display","block");
-	}
+	function cacherP(){
+	$("#formP").css("display","none"),
+	$("#formR").css("display","block"),
+	$("#btn_rechercher").css("background-color","f3cbc4"),//Quand on clique sur le boutoun rechercher "cacherP", le texte devient vert.
+	$("#btn_proposer").css("background-color","white") //Quand on clique sur le boutoun rechercher "cacherP", le texte devient noir.
+	}	
